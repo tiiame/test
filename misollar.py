bc2a4afc34f1af780475410139c0888ff2bf9665
tiiame <atavullayevsuhrob@gmail.com>
2023-11-01 20:28:48 +0200

    element kodlari uchun

--- misollar.py
+++ misollar.py
@@ -2,6 +2,12 @@
 
 
 def Juft(a):
+    # bu juft sonlikni aniqlash uchun funksiya
     if a % 2 == 0:
         return True
     return False
+
+b = [2,3,4,20]
+
+result = list(map(lambda a: a**3, b))
+print(result)
